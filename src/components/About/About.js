0e5ba4b5fa82a4947cfa8cef227096c7cb001d08
Safiamoon <msellek.safia@gmail.com>
2021-06-14 19:19:07 +0200

    updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,30 +17,29 @@ class About extends Component{
                                 <Col className="col-12 col-md-8 align-self-center">
                                     <h1 className="display-5 font-weight-bold" style={{color: "#32CD32"}}>QUI SOMMES-NOUS ?</h1>
                                     <br />
-                                    <h2>Services d’enlèvement de véhicule dans tous les départements d'Ile-de-France : 75, 77, 78, 91, 92, 93, 94, 95.</h2>
+                                    <h2>Services d’enlèvement de véhicule dans tous les départements d'Ile-de-France : 75, 77, 78, 91, 92, 93, 94, 95,100% gratuit.</h2>
                                     <p className="text-justify" style={{color:`#0C2340`}}>
-                                    Nos epavistes gratuits se hâtent pour vous 7 jours sur 7. <br/>
-                                    Nous répétons encore, nos services sont tout à fait 100% gratuits, alors, n'hésitez plus et appelez notre numéro vert pour tout enlèvement de véhicule, 
-                                    nous serons à votre écoute pour tout enlèvement d'epave.<br/>
-                                    Vous habitez en région parisienne ? Vous possèdez une voiture epave stationnée dans un arrondissement de Paris,
+                                    <br/>
+                                    Vous êtes gêné par le stationnement d’une voiture et vous souhaitez vous en débarrasser ?<br/>
+                                    FDCR'S RECYCLE intervient gratuitement sur votre lieu de prise en charge pour faire enlever votre véhicule hors d'usage ( VHU ).<br/>
+                                    Vous habitez en région parisienne ? Vous possèdez une voiture épave stationnée dans un arrondissement de Paris,
                                     châtelet, 8ème, 10ème, 11ème, 18ème arrondissement ?<br/>
-                                    Un enlèvement d'epave 75 Paris est alors nécessaire, et nous intervenons dans tous les arrondissements parisiens.<br/>
+                                    Un enlèvement d'épave 75 Paris est alors nécessaire, et nous intervenons dans tous les arrondissements parisiens.<br/>
                                     Le département du 77 est le département le plus grand d'Ile-de-France, 
                                     il est pas forcement le plus peuplé, mais constitue de nombreuses voitures hors d'usage, 
-                                    nous sommes alors très sollicités pour un enlèvement epave dans le 77 Seine-et-Marne, exemple :
+                                    nous sommes alors très sollicités pour un enlèvement épave dans le 77 Seine-et-Marne, exemple :
                                     Chelles, Meaux, Melun, Pontault-Combault, et bien plus encore !<br/>
-                                    Dans le 78 et ses villes, il y a de nombreuses épaves, alors que pensez-vous de faire appel à un epaviste pour un enlèvement d'epave gratuit dans le 78 Yvelines, exemple : Versailles, Saint-germain-en-laye, Marly-le-roi, etc.)
-                                    Savez-vous qu'il existe de nombreuses épaves dans le 91 en vue de sa superficie ? 
-                                    Pas de panique, Rapid'Epave intervient rapidement pour tout enlèvement d'epave 91 Essonne ou voiture encombrante.<br/>
+                                    Dans le 78 et ses villes, il y a de nombreuses épaves, alors que pensez-vous de faire appel à un épaviste pour un enlèvement d'épave gratuit dans le 78 Yvelines, exemple : Versailles, Saint-germain-en-laye, Marly-le-roi, etc.)
                                     Les hauts-de-seine, un petit département possèdant beaucoup de belles épaves,
-                                    nous sommes ainsi attentifs à vos appel pour organiser un enlèvement epave 92, exemple :<br/>
+                                    nous sommes ainsi attentifs à vos appel pour organiser un enlèvement épave 92, exemple :
                                     Suresnes, Boulogne-billancourt, Clichy, Colombes et bien plus encore...<br/>
                                     Nous procédons également à l'enlèvement d'epave 93, Seine-Saint-Denis. Il existe de nombreuses epaves à aubervilliers, 
                                     Saint-denis ou bien Aulnay-sous-bois, etc.<br/>
                                     Le Val-de-Marne possède aussi des epaves roulantes et non-roulantes,
-                                    nous intervenons souvent pour un enlèvement d'epave 94, dans Créteil, Vitry, 
+                                    nous intervenons souvent pour un enlèvement d'épave 94, dans Créteil, Vitry, 
                                     Chevilly-la-rue et toutes les autres villes du Val-de-marne.<br/>
-                                    Nous intervenons pour l'enlèvement d'epave dans le 95 val d'oise, à argenteuil, garges-les-gonesse ou bien Bezons par exemple.
+                                    Nous intervenons pour l'enlèvement d'épave dans le 95 val d'oise, à argenteuil, garges-les-gonesse ou bien Bezons par exemple.<br/>
+                                    Demandez l’intervention gratuite de nos épavistes professionnels agréés, pour un retrait immédiat sous 24H. <br/>
                                     </p>
                                 </Col>
                             </Row>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,17 +7,18 @@ function Home() {
     
     return (
         <>
-            <div className="row p-0 pt-3" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.80)),url('${landingImage}')`, backgroundRepeat:"no-repeat", minHeight:"92vh", width:"100%"}}>
+            <div className="row p-0 m-0" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.80)),url('${landingImage}')`, backgroundRepeat:"no-repeat", minHeight:"95vh", width:"100%"}}>
                 <div className="col-md-12 d-flex flex-column justify-content-center align-items-center text-white font-weight-bold" style={{width:"100%"}}>
                     <Zoom>
-                    <h1 className="justify-content-center align-items-center font-weight-bold" style={{color: "#32CD32"}}>ÉPAVISTE CENTRE VHU AGRÉÉ 100% GRATUIT</h1>
+                        <h1 className="display-3 pt-4 text-center justify-content-center align-items-center font-weight-bold" style={{color: "#32CD32"}}>ENLÈVEMENT ÉPAVE 100% GRATUIT</h1>
+                        <h1 className="display-4 text-center justify-content-center align-items-center font-weight-bold text-white" >ÉPAVISTE AGRÉÉ CENTRE VHU</h1>
                         <h3 className="btn-group d-inline mt-5 font-weight-bold text-white">
                         Du lundi au samedi de 10h a 18h
                         </h3>
-                        <h3 className="btn-group d-inline mt-2" style={{color: "#32CD32"}}>
+                        <h3 className="btn-group d-inline mt-2 font-weight-bold" style={{color: "#32CD32"}}>
                         📞 +33 69 58 32 98 7 
                         </h3>
-                        <h3 className="btn-group d-inline mt-2">
+                        <h3 className="btn-group d-inline mt-2 font-weight-bold">
                         📍 Île-de-France
                         </h3>
                     </Zoom>
