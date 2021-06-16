db0969c201622959ace4541c56f2dba2fc472747
Safiamoon <msellek.safia@gmail.com>
2021-06-16 10:06:35 +0200

    Updates

---
+++ precache-manifest.1779883eff071f635b19bb655512aaf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "605b140400a39cf64531",
+    "url": "/epave/static/css/main.54bded86.chunk.css"
+  },
+  {
+    "revision": "605b140400a39cf64531",
+    "url": "/epave/static/js/main.8dde3796.chunk.js"
+  },
+  {
+    "revision": "8a3766cb9c727249f959",
+    "url": "/epave/static/js/runtime~main.a524bbed.js"
+  },
+  {
+    "revision": "1766840e06d8d80e1666",
+    "url": "/epave/static/js/2.8e88d8b3.chunk.js"
+  },
+  {
+    "revision": "a5dbe9711d43a91052fc275579303f27",
+    "url": "/epave/static/media/phone.a5dbe971.jpg"
+  },
+  {
+    "revision": "6233ddd5a27cb61e5f2659448be63d9a",
+    "url": "/epave/static/media/5.6233ddd5.jpg"
+  },
+  {
+    "revision": "4608eed1fdf3459b2a6e067b0285421c",
+    "url": "/epave/static/media/green.4608eed1.jpg"
+  },
+  {
+    "revision": "649e2fdc1bd41c6a4036f428133cc970",
+    "url": "/epave/static/media/logo5.649e2fdc.PNG"
+  },
+  {
+    "revision": "042ae5c066e8865f17c803757d5ca43d",
+    "url": "/epave/static/media/carRecycle2.042ae5c0.jpg"
+  },
+  {
+    "revision": "a5a9393aec1a8682eca3346ff50a2bee",
+    "url": "/epave/static/media/location.a5a9393a.PNG"
+  },
+  {
+    "revision": "691471f2c1991bd5e36df552bf084de2",
+    "url": "/epave/index.html"
+  }
+];
